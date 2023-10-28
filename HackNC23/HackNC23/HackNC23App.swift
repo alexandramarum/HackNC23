ffb1bb21685562ba6b96ffa8d358cb5a60490095
alexandramarum <alexandramarum@gmail.com>
2023-10-28 11:39:14 -0400

    Xcode

---
+++ HackNC23/HackNC23/HackNC23App.swift
@@ -0,0 +1,17 @@
+//
+//  HackNC23App.swift
+//  HackNC23
+//
+//  Created by Alexandra Marum on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HackNC23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
