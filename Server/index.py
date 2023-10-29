fa45c97978738fd352433b215f03b6048fdc6eb2
alexandramarum <alexandramarum@gmail.com>
2023-10-28 20:31:46 -0400

    py server

--- Server/index.py
+++ Server/index.py
@@ -1,52 +1,29 @@
-from dotenv import load_dotenv
+from flask import Flask, request, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import os
-
 
 CLIENT_ID = "4b75fc2e93ff43ac826df339a8d61e43"
 CLIENT_SECRET = "b4e2df89183544a5b8c1880ee41acc99"
 REDIRECT_URI = "http://localhost:5000/callback"
 
-
-
+# Initialize the Flask app
+app = Flask(__name__)
 
 # Initialize the Spotify API client
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-   client_id=CLIENT_ID,
-   client_secret=CLIENT_SECRET,
-   redirect_uri=REDIRECT_URI,
-   scope='user-library-read'  # Permission to access the user's library
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope='user-library-read'  # Permission to access the user's library
 ))
 
+@app.route('/callback', methods=['GET'])
+def callback():
+    # Get the access token after successful Spotify login
+    sp.auth_manager.get_access_token(request.args['code'])
 
-# Get the current user's playlists
-user_playlists = sp.current_user_playlists()
+    # Redirect to the front-end URL (you should change this URL to match your Swift app's callback)
+    return redirect('YOUR_SWIFT_UI_REDIRECT_URL')
 
-
-# Initialize variables to keep track of the longest playlist
-longest_playlist_name = ""
-longest_playlist_track_count = 0
-
-
-# Find the longest playlist
-for playlist in user_playlists['items']:
-   playlist_name = playlist['name']
-   playlist_id = playlist['id']
-  
-   # Get the number of tracks in the playlist
-   playlist_tracks = sp.playlist_tracks(playlist_id)
-   track_count = playlist_tracks['total']
-
-
-   # Check if this playlist is longer than the current longest
-   if track_count > longest_playlist_track_count:
-       longest_playlist_name = playlist_name
-       longest_playlist_track_count = track_count
-
-
-# Print the longest playlist
-if longest_playlist_name:
-   print(f"The longest playlist is '{longest_playlist_name}' with {longest_playlist_track_count} tracks.")
-else:
-   print("No playlists found.")
+if __name__ == '__main__':
+    app.run(debug=True)
